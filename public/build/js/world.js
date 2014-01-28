abb187b2178be678131733950411d35c63274069
Eric <Ericrius1@gmail.com>
2014-01-28 07:10:18 -0800

    add worls"

--- public/build/js/world.js
+++ public/build/js/world.js
@@ -4,18 +4,33 @@ var World,
 FW.World = World = (function() {
   function World() {
     this.animate = __bind(this.animate, this);
+    var aMeshMirror, waterNormals;
     FW.clock = new THREE.Clock();
     this.SCREEN_WIDTH = window.innerWidth;
     this.SCREEN_HEIGHT = window.innerHeight;
     FW.Width = 100000;
     this.camFar = FW.width * 2;
     this.time = Date.now();
+    this.rippleFactor = 90;
     FW.camera = new THREE.PerspectiveCamera(50.0, this.SCREEN_WIDTH / this.SCREEN_HEIGHT, 1, this.camFar);
     FW.scene = new THREE.Scene();
     FW.Renderer = new THREE.WebGLRenderer();
     FW.Renderer.setSize(this.SCREEN_WIDTH, this.SCREEN_HEIGHT);
     FW.Renderer.setClearColor(0x06071a);
     document.body.appendChild(FW.Renderer.domElement);
+    waterNormals = new THREE.ImageUtils.loadTexture('/assets/waternormals.jpg');
+    waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+    this.water = new THREE.Water(FW.Renderer, FW.camera, FW.scene, {
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals: waterNormals,
+      alpha: 1.0,
+      distortionScale: 20
+    });
+    aMeshMirror = new THREE.Mesh(new THREE.PlaneGeometry(FW.width, FW.width, 50, 50), this.water.material);
+    aMeshMirror.add(this.water);
+    aMeshMirror.rotation.x = -Math.PI * 0.5;
+    FW.scene.add(aMeshMirror);
   }
 
   World.prototype.animate = function() {
@@ -23,12 +38,14 @@ FW.World = World = (function() {
     requestAnimationFrame(this.animate);
     delta = FW.clock.getDelta();
     time = Date.now();
+    this.water.material.uniforms.time.value += 1.0 / this.rippleFactor;
     return this.render();
   };
 
   World.prototype.render = function() {
     var delta;
     delta = FW.clock.getDelta();
+    this.water.render();
     return FW.Renderer.render(FW.scene, FW.camera);
   };
 
--- public/coffee/world.coffee
+++ public/coffee/world.coffee
@@ -6,6 +6,7 @@ FW.World = class World
     FW.Width = 100000
     @camFar = FW.width * 2
     @time = Date.now()
+    @rippleFactor = 90
 
     # CAMERA
     FW.camera = new THREE.PerspectiveCamera(50.0, @SCREEN_WIDTH / @SCREEN_HEIGHT, 1, @camFar)
@@ -19,12 +20,31 @@ FW.World = class World
     FW.Renderer.setClearColor 0x06071a
     document.body.appendChild FW.Renderer.domElement
 
+    #WATER
+    waterNormals = new THREE.ImageUtils.loadTexture '/assets/waternormals.jpg'
+    waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
+    @water = new THREE.Water FW.Renderer, FW.camera, FW.scene,
+      textureWidth: 512
+      textureHeight: 512
+      waterNormals: waterNormals
+      alpha: 1.0
+      distortionScale: 20
+    aMeshMirror = new THREE.Mesh(
+      new THREE.PlaneGeometry FW.width, FW.width, 50, 50
+      @water.material
+    )
+    aMeshMirror.add @water
+    aMeshMirror.rotation.x = -Math.PI * 0.5
+    FW.scene.add aMeshMirror
+
   animate : =>
     requestAnimationFrame @animate
     delta = FW.clock.getDelta()
     time = Date.now()
+    @water.material.uniforms.time.value += 1.0 / @rippleFactor
     @render()
 
   render : ->
     delta = FW.clock.getDelta()
+    @water.render()
     FW.Renderer.render(FW.scene, FW.camera)
