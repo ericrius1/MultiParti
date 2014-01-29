105433f38f30ea42b1621b64b7a2d97d13e63650
Eric <Ericrius1@gmail.com>
2014-01-28 20:52:45 -0800

    spa

--- public/build/js/main.js
+++ public/build/js/main.js
@@ -2,7 +2,6 @@ window.FW = {};
 
 $(document).ready(function() {
   var Main;
-  $('.hi').css('color', 'red');
   return FW.main = Main = (function() {
     function Main() {
       FW.myWorld = new FW.World();
--- public/coffee/main.coffee
+++ public/coffee/main.coffee
@@ -1,6 +1,7 @@
 window.FW = {}
 $(document).ready ->
-  $('.hi').css('color', 'red')  
+
+
   FW.main = class Main
     constructor: ->
     	FW.myWorld = new FW.World()
